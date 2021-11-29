838bc347af07acc647a1abddd4c64f28e4a90704
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2021-11-29 12:27:00 -0300

    Crea index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+function sumar(numero1, numero2) {
+  return numero1 + numero2;
+}
+
+function restar(numero1, numero2) {
+  numero1 - numero2;
+}
+
+function multiplicar(numero1, numero2) {
+  numero1 * numero2;
+}
+
+function dividir(numero1, numero2) {
+  if (numero2 === 0) {
+    console.log("No se puede dividir por 0");
+  } else {
+    return numero1 / numero2;
+  }
+}
+
+console.log(sumar(5, 5));
