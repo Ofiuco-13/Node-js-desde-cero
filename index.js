1f8e02d8c14ca09d0e2767a63c987add9154a52f
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2021-11-29 12:41:31 -0300

    Importa matematica.js

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-function sumar(numero1, numero2) {
-  return numero1 + numero2;
-}
+const matematica = require("./matematica.js");
 
-function restar(numero1, numero2) {
-  numero1 - numero2;
-}
-
-function multiplicar(numero1, numero2) {
-  numero1 * numero2;
-}
-
-function dividir(numero1, numero2) {
-  if (numero2 === 0) {
-    console.log("No se puede dividir por 0");
-  } else {
-    return numero1 / numero2;
-  }
-}
-
-console.log(sumar(5, 5));
+console.log(matematica.dividir(5, 0));
